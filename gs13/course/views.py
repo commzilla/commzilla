0e7441bac5bb46f615045336ed2fa4a17300d7a5
commzilla <95210777+commzilla@users.noreply.github.com>
2021-03-27 19:27:56 +0600

    Delete views.py

--- gs13/course/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def learn_django_course(request):
-    return render(request, 'course/home.html', {'title': 'Learn Django','cname':'Django'})
