26d2840eb45281d2a58c5349950e8193106dc136
commzilla <95210777+commzilla@users.noreply.github.com>
2021-03-30 19:28:21 +0600

    Delete views.py

--- gs14/course/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def course_list(request):
-    details = {'b': 300, 'e': 400, 'm': 500, 's':500}
-    return render(request, 'course/home.html', context=details)
