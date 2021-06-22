06ddee273f72bb936647bc30f17dca930cccbf16
commzilla <95210777+commzilla@users.noreply.github.com>
2021-06-22 19:42:30 +0600

    Delete views.py

--- gs9/fee/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def fee_django(request):
-    return HttpResponse("<h1>Hello Django! fee</h1>")
-
-def fee_python(request):
-    return HttpResponse("<h1>Hello Python fee</h1>")
