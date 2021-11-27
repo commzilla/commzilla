360bdd85855eacd6f973fdbf42d077b28ba95f4d
commzilla <95210777+commzilla@users.noreply.github.com>
2021-11-27 20:13:34 +0600

    Create views.py

---
+++ Django_Learn_3/fees/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def fees_django(request):
+    return HttpResponse("Hello fees_django")
+
+def fees_python(request):
+    return HttpResponse("Hello fees_python")
