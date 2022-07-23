0abca6db2526c08ddf35ccbfda6156fd4e33c0df
commzilla <95210777+commzilla@users.noreply.github.com>
2022-07-23 15:43:39 +0600

    Delete views.py

--- Django_Learn_3/course/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-def Learn_django(request):
-    return HttpResponse("Hello Learn_Django")
-
-def Learn_python(request):
-    return HttpResponse("Hello Learn_Python")
