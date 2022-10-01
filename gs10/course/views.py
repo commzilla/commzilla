a8f9b0851f03c4d6c4df100e0c9239f87248a1ad
commzilla <95210777+commzilla@users.noreply.github.com>
2022-10-01 15:55:23 +0600

    Delete views.py

--- gs10/course/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def bangla(request):
-    return render(request,'course/courseOne.html')
-
-def english(request):
-    return render(request,'course/courseTwo.html')
