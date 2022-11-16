828c1b538868421fd24d3ac90f3f64b37fe793e1
commzilla <95210777+commzilla@users.noreply.github.com>
2022-11-16 16:01:13 +0600

    Delete urls.py

--- gs11/gs11/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path,include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('c/', include('course.urls')),
-    path('f/', include('fees.urls'))
-]
